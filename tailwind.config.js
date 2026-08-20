cc20165a1b461281de67f7133f714eb478bd62f4
alanshahd82-dotcom <alanshahd82@gmail.com>
2026-08-20 16:18:07 +0100

    Add core navy and gold Tailwind colors

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: '#0A192F',
+          800: '#243b53',
+          700: '#334e68',
+        },
+        gold: {
+          500: '#D4AF37',
+          400: '#ffcc33',
+          600: '#b8941f',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
